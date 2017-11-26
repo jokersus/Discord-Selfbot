3b736196ccc16f1b919301f018162acc1433c6db
pallavbs2 <33978126+pallavbs2@users.noreply.github.com>
2017-11-26 09:19:18 -0800

    even more fstrings

--- cogs/utility.py
+++ cogs/utility.py
@@ -838,7 +838,7 @@ class Utility:
         username = await git.githubusername()
         async with self.session.get('https://api.github.com/repos/appu1232/Discord-Selfbot/git/refs/heads/master', headers={"Authorization": "Bearer {}".format(git.githubtoken)}) as resp:
             if 300 > resp.status >= 200:
-                async with self.session.post(f'https://api.github.com/repos/{username}/Discord-Selfbot/merges', json={"head": (await resp.json())['object']['sha'], "base": "master", "commit_message": "Updating Bot"}, headers={"Authorization": "Bearer {}".format(git.githubtoken)}) as resp2:
+                async with self.session.post('https://api.github.com/repos/{}/Discord-Selfbot/merges'.format(username), json={"head": (await resp.json())['object']['sha'], "base": "master", "commit_message": "Updating Bot"}, headers={"Authorization": "Bearer {}".format(git.githubtoken)}) as resp2:
                     if 300 > resp2.status >= 200:
                         if resp2.status == 204:
                             return await ctx.send('Already at latest version!')
